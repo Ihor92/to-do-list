a14a932148627ca8a7b3503a1d44ed8cd910fc85
Ihor92 <salfetnikov92@gmail.com>
2020-03-12 13:57:39 +0200

    Updates

---
+++ precache-manifest.a890fe9f159c3ce5d1f3bb1376b97a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60735ac65499f7c1c98f2a50a3e6243c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "ca4f5f7b54529298770e",
+    "url": "/to-do-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "625f18ba012afd417d72",
+    "url": "/to-do-list/static/js/2.1092ba8f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to-do-list/static/js/2.1092ba8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4f5f7b54529298770e",
+    "url": "/to-do-list/static/js/main.0693cfb4.chunk.js"
+  },
+  {
+    "revision": "b33ea1fa2caca91076bf",
+    "url": "/to-do-list/static/js/runtime-main.bf3648d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
